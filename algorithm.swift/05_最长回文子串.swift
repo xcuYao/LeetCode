14e65f39a66546a8883b48e8b06faec9994be455
yaoning <chinayaoning@gmail.com>
2020-01-10 02:32:05 +0800

    update: 最长回文字串 中心扩散法求解

--- algorithm.swift/05_最长回文子串.swift
+++ algorithm.swift/05_最长回文子串.swift
@@ -25,13 +25,14 @@ import Foundation
 //著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 class Solution05 {
-    
+
     // 解法1 暴力求解
     // 增加判断是否是回文字符串的方法 isPalindrome
     // 两层循环 找出所有可能的子字符串
     // 所有的子字符串去检查是否是回文
     // 永远记录最长值
     // 遍历完毕 返回最长值
+    // 时间复杂度O(n^3)
 //    func isPalindrome(_ s: String) -> Bool {
 //        let characters = Array(s)
 //        for index in 0..<characters.count / 2 {
@@ -58,14 +59,45 @@ class Solution05 {
 //        }
 //        return ans
 //    }
-    
-    // 解法2 马拉车法
+
+    var res = ""
+
+    // 解法2 中心扩散法
+    // 存在两种回文的情况 aba abba
+    // 所以从中心扩散 可能为(i,i) 即一个字母 b
+    //             也可能为(i, i+1) 即两个字母 bb
+    // 遍历一边 针对两种情况 分别调用helper方法 检查回文 找出长度最长的那一个
+    // helper方法 就判断left和rigth是否相同即可，相同left左移 rigth右移
+    // 不同即可结束
+    // 时间复杂度O(n^2) 空间复杂度O(1)
     func longestPalindrome(_ s: String) -> String {
-        return ""
+        if s.count == 0 {
+            return s
+        }
+        let characters = Array(s)
+        for i in 0..<characters.count {
+            helper(s: s, left: i, right: i)
+            helper(s: s, left: i, right: i + 1)
+        }
+        return res
     }
-    
+
+    func helper(s: String, left: Int, right: Int) {
+        var l = left
+        var r = right
+        let characters = Array(s)
+        while l >= 0 && r < s.count && characters[l] == characters[r] {
+            l -= 1
+            r += 1
+        }
+        let cur = String(characters[l + 1..<r])
+        if cur.count > res.count {
+            res = cur
+        }
+    }
+
     static func test() {
-        let s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+        let s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
         let res = Solution05().longestPalindrome(s)
         print("\(res)")
     }
