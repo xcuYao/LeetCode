265a219b07e8930b540ea1afb32ba18afe88704e
yaoning <chinayaoning@gmail.com>
2020-01-19 20:45:38 +0800

    update: 加油站问题 解法2

--- algorithm.swift/Array/134_加油站.swift
+++ algorithm.swift/Array/134_加油站.swift
@@ -9,18 +9,35 @@
 import Foundation
 
 class Solution134 {
+//    func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
+//        var spare = 0
+//        var minSpare = Int.max
+//        var minIndex = 0
+//        for i in 0..<gas.count {
+//            spare += gas[i] - cost[i]
+//            if spare < minSpare {
+//                minSpare = spare
+//                minIndex = i
+//            }
+//        }
+//        return spare < 0 ? -1 : (minIndex + 1) % gas.count
+//    }
+
     func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
-        var spare = 0
-        var minSpare = Int.max
-        var minIndex = 0
+        if gas.count == 0 || cost.count == 0 { return -1 }
+        var total = 0
+        var curr = 0
+        var start = 0
+
         for i in 0..<gas.count {
-            spare += gas[i] - cost[i]
-            if spare < minSpare {
-                minSpare = spare
-                minIndex = i
+            curr += gas[i] - cost[i]
+            if curr < 0 {
+                start = i + 1
+                curr = 0
             }
+            total += gas[i] - cost[i]
         }
-        return spare < 0 ? -1 : (minIndex + 1) % gas.count
+        return total >= 0 ? start : -1
     }
 
     static func test() {
