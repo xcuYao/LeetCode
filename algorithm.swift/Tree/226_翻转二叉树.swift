472a8a467ca6557f17f7a75dd87880f0ba7b05cf
yaoning <chinayaoning@gmail.com>
2020-06-04 15:30:58 +0800

    update: 二叉树路径

--- algorithm.swift/Tree/226_翻转二叉树.swift
+++ algorithm.swift/Tree/226_翻转二叉树.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Solution226 {
+
+    public class TreeNode {
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+        }
+    }
+
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil { return nil }
+        let left = invertTree(root?.left)
+        let right = invertTree(root?.right)
+        root?.left = right
+        root?.right = left
+        return root
+    }
+}
--- algorithm.swift/Tree/257_二叉树的所有路径.swift
+++ algorithm.swift/Tree/257_二叉树的所有路径.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class Solution257 {
+
+    public class TreeNode {
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+        }
+    }
+
+    func construct_path(root: TreeNode?, path: String, paths: inout [String]) {
+        guard let r = root else { return }
+        var p = path.appending(String(r.val))
+        if r.left == nil && r.right == nil {
+            paths.append(p)
+        } else {
+            p = p.appending("->")
+            construct_path(root: r.left, path: p, paths: &paths)
+            construct_path(root: r.right, path: p, paths: &paths)
+        }
+    }
+
+    func binaryTreePaths(_ root: TreeNode?) -> [String] {
+        let path = ""
+        var paths: [String] = []
+        construct_path(root: root, path: path, paths: &paths)
+        return []
+    }
+
+}
